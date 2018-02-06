21b5bd4ad8abe4fc619e86d4a9d9d93e511f4319
mdrisk <mrisk@westclarkschools.com>
2018-02-05 20:06:05 -0500

    home commit 2-5

--- src/index.js
+++ src/index.js
@@ -15,10 +15,16 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos}) //Same as this.setState({ videos : videos })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      }); //Same as this.setState({ videos : videos })
     });
   }
 
@@ -26,10 +32,12 @@ class App extends Component {
     return (
     <div>
       <SearchBar />
-      <VideoDetail video={this.state.videos[0]} />
-      <VideoList videos={this.state.videos} />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+        onVideoSelect = { selectedVideo => this.setState({ selectedVideo }) }
+        videos={ this.state.videos } />
     </div>
-    );
+  );
   }
 }
 
